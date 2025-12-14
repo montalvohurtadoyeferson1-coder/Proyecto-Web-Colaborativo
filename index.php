404983be1fb1b339d2704e96739499b79e69d372
nayda <jesusnayda15@gmail.com>
2025-12-14 05:01:16 +0100

    Solucionar barra de navegación duplicada

--- index.php
+++ index.php
@@ -7,8 +7,10 @@
     <link rel="stylesheet" href="css/estilos.css">
 </head>
 <body>
+    
     <header>
         <img src="img/logo.png" alt="Logo" class="logo">
+        
         <nav>
             <ul>
                 <li><a href="index.php">Inicio</a></li>
@@ -23,20 +25,13 @@
         <div class="banner">
             <img src="img/banner.jpeg" alt="Banner">
         </div>
-    <nav>
-        <ul>
-            <li><a href="index.php">Inicio</a></li>
-            <li><a href="about.php">Sobre Nosotros</a></li>
-            <li><a href="contact.php">Contacto</a></li>
-            <li><a href="register.php">Registro</a></li>
-        </ul>
-    </nav>
-    
-    <div class="container">
-        <h1>Bienvenido al Proyecto Web Colaborativo</h1>
-        <p>Esta es la página principal de nuestro proyecto.</p>
-        <p>Desarrollado con XAMPP, VS Code y GitHub.</p>
-    </div>
+        
+        <div class="container">
+            <h1>Bienvenido al Proyecto Web Colaborativo</h1>
+            <p>Esta es la página principal de nuestro proyecto.</p>
+            <p>Desarrollado con XAMPP, VS Code y GitHub.</p>
+        </div>
+    </main>
     
     <script src="js/funciones.js"></script>
 </body>
