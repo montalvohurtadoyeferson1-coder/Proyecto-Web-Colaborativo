5b8a7df0a958391ca9d7c4763820ca99c320b7e5
yeferson <montalvohurtadoyeferson1@gmail.com>
2025-12-16 09:10:16 -0500

    trabajo colaborativo modificado

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['login_submit'])) {
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>Proyecto Web Colaborativo</title>
+    <title>MI PAGINA 2.0</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/estilos.css">
 </head>
